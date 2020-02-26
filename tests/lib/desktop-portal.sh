16879124ce9a053eaf3a06898cf8ae595f6a4f23
Restyled.io <commits@restyled.io>
2020-02-26 06:04:43 +0000

    Restyled by shfmt

--- tests/lib/desktop-portal.sh
+++ tests/lib/desktop-portal.sh
@@ -7,16 +7,16 @@
 . "$TESTSLIB"/user.sh
 
 setup_portals() {
-    # Install xdg-desktop-portal and configure service activation for
-    # fake portal UI.
-    distro_install_package xdg-desktop-portal
-    cat << EOF > /usr/share/dbus-1/services/org.freedesktop.impl.portal.spread.service
+  # Install xdg-desktop-portal and configure service activation for
+  # fake portal UI.
+  distro_install_package xdg-desktop-portal
+  cat <<EOF >/usr/share/dbus-1/services/org.freedesktop.impl.portal.spread.service
 [D-BUS Service]
 Name=org.freedesktop.impl.portal.spread
 Exec=/usr/bin/python3 $TESTSLIB/fakeportalui/portalui.py
 SystemdService=spread-portal-ui.service
 EOF
-    cat << EOF > /usr/lib/systemd/user/spread-portal-ui.service
+  cat <<EOF >/usr/lib/systemd/user/spread-portal-ui.service
 [Unit]
 Description=Fake portal UI
 [Service]
@@ -24,27 +24,27 @@ Type=dbus
 BusName=org.freedesktop.impl.portal.spread
 ExecStart=/usr/bin/python3 $TESTSLIB/fakeportalui/portalui.py
 EOF
-    mkdir -p /usr/share/xdg-desktop-portal/portals
-    cat << EOF > /usr/share/xdg-desktop-portal/portals/spread.portal
+  mkdir -p /usr/share/xdg-desktop-portal/portals
+  cat <<EOF >/usr/share/xdg-desktop-portal/portals/spread.portal
 [portal]
 DBusName=org.freedesktop.impl.portal.spread
 Interfaces=org.freedesktop.impl.portal.FileChooser;org.freedesktop.impl.portal.Screenshot;org.freedesktop.impl.portal.AppChooser;
 UseIn=spread
 EOF
 
-    start_user_session
-    as_user systemctl --user set-environment XDG_CURRENT_DESKTOP=spread
+  start_user_session
+  as_user systemctl --user set-environment XDG_CURRENT_DESKTOP=spread
 }
 
 teardown_portals() {
-    stop_user_session
+  stop_user_session
 
-    rm -f /usr/share/dbus-1/services/org.freedesktop.impl.portal.spread.service
-    rm -f /usr/lib/systemd/user/spread-portal-ui.service
-    rm -f /usr/share/xdg-desktop-portal/portals/spread.portal
+  rm -f /usr/share/dbus-1/services/org.freedesktop.impl.portal.spread.service
+  rm -f /usr/lib/systemd/user/spread-portal-ui.service
+  rm -f /usr/share/xdg-desktop-portal/portals/spread.portal
 
-    distro_purge_package xdg-desktop-portal
-    distro_auto_remove_packages
+  distro_purge_package xdg-desktop-portal
+  distro_auto_remove_packages
 
-    purge_user_session_data
+  purge_user_session_data
 }
